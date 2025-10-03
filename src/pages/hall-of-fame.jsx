926f133fe29e0ecc90d1f022db6815d71b670811
moaalk <mohamed.asath@ifs.com>
2025-10-03 18:23:55 +0530

    Feat: Update Hall of Fame to display 2024 leaderboard data

--- src/pages/hall-of-fame.jsx
+++ src/pages/hall-of-fame.jsx
@@ -2,12 +2,14 @@ import ScoreBoard from "@/components/home/score-board";
 import { useFetchPastLeaderboardsQuery } from "@/store/api";
 
 const HallOfFame = () => {
+  const useFetchData = (params) => useFetchPastLeaderboardsQuery({ ...params, year: 2024 });
+
   return (
     <ScoreBoard
       pageTitle={"Hall of Fame | Bashaway"}
-      title={"Hall of Fame 2023"}
+      title={"Hall of Fame 2024"}
       subTitle={" A tribute to legendary warriors who once marched amongst us with unwavering valour"}
-      useFetchData={useFetchPastLeaderboardsQuery}
+      useFetchData={useFetchData}
     />
   );
 };
